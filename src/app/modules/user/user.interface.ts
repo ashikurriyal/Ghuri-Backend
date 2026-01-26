6948be51978fb0a80bc89bbaa140e854f8c5b1b8
ashikurriyal <riyal.ashikur@gmail.com>
2026-01-27 03:31:15 +0600

    creating user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,36 @@
+import mongoose = require("mongoose");
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+}
+
+//authProviders = emailpassword, google auth,
+export interface IAuthProvider {
+  provider: string; //Google, Credential
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified?: string;
+  role: Role;
+  auths: IAuthProvider[];
+  bookings: mongoose.Types.ObjectId[];
+  guides?: mongoose.Types.ObjectId[];
+}
