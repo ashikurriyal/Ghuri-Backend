0a0a128624f450d9628fcc341f7883fc5bd4e951
ashikurriyal <riyal.ashikur@gmail.com>
2026-01-27 04:05:27 +0600

    creating user controller and routing

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import Express = require("express");
+import { UserRoutes } from "./app/modules/user/user.route";
+import cors from "cors";
+
 const app = Express();
+app.use(Express.json());
+app.use(cors());
+
+
+app.use("/api/v1/user", UserRoutes);
 
 app.get("/", (req: Express.Request, res: Express.Response) => {
   res.status(200).json({
