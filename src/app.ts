2c9fa41d8912b9e0264825338ce36e6166370e48
ashikurriyal <riyal.ashikur@gmail.com>
2026-01-27 08:45:49 +0600

    creating user and organizing routing system

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import Express = require("express");
-import { UserRoutes } from "./app/modules/user/user.route";
 import cors from "cors";
+import { router } from "./app/Routes";
 
 const app = Express();
 app.use(Express.json());
 app.use(cors());
 
 
-app.use("/api/v1/user", UserRoutes);
+app.use("/api/v1", router);
 
 app.get("/", (req: Express.Request, res: Express.Response) => {
   res.status(200).json({
