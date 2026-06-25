cbdd009b4fc10b7d245d86c33623ee036878c12b
ashikurriyal <riyal.ashikur@gmail.com>
2026-06-26 04:17:58 +0600

    add custom property to request parameter of express and finished the credential based authentication sytem

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
 
 import { checkAuth } from "../../middlewares/checkAuth";
@@ -10,7 +10,7 @@ const router = Router();
 
 router.post("/register", validateRequest(createUserZodSchema), UserControllers.createUser);
 router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers);
-router.patch("/:id", checkAuth(...Object.values(Role)), UserControllers.updateUser)
+router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
 
 
 export const UserRoutes = router;
