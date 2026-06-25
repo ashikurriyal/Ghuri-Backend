c359ba920886dc84659dae17c069cecd1a98c3c1
ashikurriyal <riyal.ashikur@gmail.com>
2026-06-26 03:54:44 +0600

    created update user api and password rehashing

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,10 @@
 import AppError from "../../errorHelpers/AppError";
-import { IAuthProvider, IUser } from "./user.interface";
+import { IAuthProvider, IUser, Role } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
 import envVars from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = async (payload: IUser) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -33,6 +34,50 @@ const createUser = async (payload: IUser) => {
   return user;
 };
 
+const updateUser = async (userId: string, payload: Partial<IUser>, decodedToken: JwtPayload) => {
+  /*
+  email - cant update
+  name, phone, address
+  password - rehashing
+  only admin superadmin - role, isDeleted...
+  promoting to superadmin - superadmin (admin cant promote superadmin)
+  */
+
+  const ifUserExist = await User.findById(userId);
+
+  if(!ifUserExist){
+    throw new AppError(httpStatus.NOT_FOUND, 'User Not Found')
+  }
+
+  // if(ifUserExist.isDeleted || ifUserExist.isActive === IsActive.BLOCKED){
+  //   throw new AppError(httpStatus.FORBIDDEN, 'This user can not be updated')
+  // }
+
+  if (payload.role) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
+    }
+
+    if (payload.role === Role.SUPER_ADMIN && decodedToken.role === Role.ADMIN) {
+      throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
+    }
+  }
+
+  if (payload.isActive || payload.isDeleted || payload.isVerified) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
+    }
+  }
+
+  if(payload.password){
+    payload.password = await bcryptjs.hash(payload.password, envVars.BCRYPT_SALT_AROUND)
+  }
+
+  const newUpdateUser = await User.findByIdAndUpdate(userId, payload, {new: true, runValidators: true})
+
+  return newUpdateUser;
+}
+
 const getAllUsers = async () => {
   const users = await User.find({});
   const totalUsers = await User.countDocuments();
@@ -48,4 +93,5 @@ const getAllUsers = async () => {
 export const UserServices = {
   createUser,
   getAllUsers,
+  updateUser
 };
