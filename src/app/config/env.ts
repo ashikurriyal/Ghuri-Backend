60f1492aa5441ea80fe509a2effc06e09215c986
ashikurriyal <riyal.ashikur@gmail.com>
2026-06-26 02:46:23 +0600

    verifying token and protecting routes using middleware and creating jwt helpers and checkAuth Middleware

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,11 +6,14 @@ interface EnvConfig {
   PORT: string;
   DB_URL: string;
   NODE_ENV: "development" | "production";
+  BCRYPT_SALT_AROUND: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_ACCESS_EXPIRES: string;
 }
 
 const loadEnvVariables = (): EnvConfig => {
 
-  const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV"];
+  const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_AROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET"];
   requiredEnvVariables.forEach(key => {
     if(!process.env[key]){
       throw new Error(`Missing require environment variable ${key}`)
@@ -21,6 +24,9 @@ const loadEnvVariables = (): EnvConfig => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     DB_URL: process.env.DB_URL!,
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
+    BCRYPT_SALT_AROUND: process.env.BCRYPT_SALT_AROUND as string,
+    JWT_ACCESS_EXPIRES:process.env.JWT_ACCESS_EXPIRES as string,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
   };
 };
 const envVars = loadEnvVariables()
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,7 +2,8 @@ import httpStatus from "http-status-codes";
 import { User } from "../user/user.model";
 import AppError from "../../errorHelpers/AppError";
 import bcryptjs from 'bcryptjs';
-import jwt from 'jsonwebtoken'
+import { generateToken } from "../../utils/jwt";
+import envVars from "../../config/env";
 
 const credentialsLogin = async (payload: { email: string; password: string }) => {
     const { email, password } = payload;
@@ -26,9 +27,11 @@ const credentialsLogin = async (payload: { email: string; password: string }) =>
         role: isUserExist.role
     }
 
-    const accessToken = jwt.sign(jwtPayload, 'secret', {
-        expiresIn: '1d'
-    })
+    // const accessToken = jwt.sign(jwtPayload, 'secret', {
+    //     expiresIn: '1d'
+    // })
+
+    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
 
     return {
         // email: isUserExist.email
