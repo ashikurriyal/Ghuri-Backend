c6c60bf4c0f56b4e8b8e553c0ef5d960da309015
ashikurriyal <riyal.ashikur@gmail.com>
2026-07-10 11:17:22 +0600

    created logout user api

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -41,13 +41,39 @@ const getNewAccessToken = catchAsync(
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
-      message: "User Logged In Successfully!",
+      message: "New Access Token Retrived Successfully!",
       data: tokenInfo,
     });
   },
 );
 
+
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax'
+    })
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax'
+    })
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Logged Out Successfully!",
+      data: null,
+    });
+  },
+);
+
+
 export const AuthControllers = {
     credentialsLogin,
-    getNewAccessToken
+    getNewAccessToken,
+    logout
 }
