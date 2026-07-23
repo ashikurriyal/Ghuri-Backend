effd010f92183a31bd765de54c1ed57614f884d5
Molla Ashikur Rahman <63509293+ashikurriyal@users.noreply.github.com>
2026-07-23 20:11:39 +0600

    Merge pull request #4 from ashikurriyal/user-module
    User module

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,17 +1,82 @@
-//api's business logic & functionalities are in service file
-
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser, Role } from "./user.interface";
 import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+import bcryptjs from "bcryptjs";
+import envVars from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = async (payload: IUser) => {
-  const { name, email } = payload;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { email, auths, password, ...rest } = payload;
+
+  const isUserExist = await User.findOne({ email })
+
+  if (isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User Already Exists");
+  }
+
+  const hashedPassword = await bcryptjs.hash(password as string, Number(envVars.BCRYPT_SALT_AROUND))
+
+  // const isPasswordMatch = await bcryptjs.compare(password as string, hashedPassword)
+
+  // console.log(isPasswordMatch)
+
+  const authProvider: IAuthProvider = { provider: 'credentials', providerId: email as string };
+
   const user = await User.create({
-    name,
     email,
+    auths: [authProvider],
+    password: hashedPassword,
+    ...rest
   });
 
   return user;
 };
+
+const updateUser = async (userId: string, payload: Partial<IUser>, decodedToken: JwtPayload) => {
+  /*
+  email - cant update
+  name, phone, address
+  password - rehashing
+  only admin superadmin - role, isDeleted...
+  promoting to superadmin - superadmin (admin cant promote superadmin)
+  */
+
+  const ifUserExist = await User.findById(userId);
+
+  if(!ifUserExist){
+    throw new AppError(httpStatus.NOT_FOUND, 'User Not Found')
+  }
+
+  // if(ifUserExist.isDeleted || ifUserExist.isActive === IsActive.BLOCKED){
+  //   throw new AppError(httpStatus.FORBIDDEN, 'This user can not be updated')
+  // }
+
+  if (payload.role) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
+    }
+
+    if (payload.role === Role.SUPER_ADMIN && decodedToken.role === Role.ADMIN) {
+      throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
+    }
+  }
+
+  if (payload.isActive || payload.isDeleted || payload.isVerified) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
+    }
+  }
+
+  if(payload.password){
+    payload.password = await bcryptjs.hash(payload.password, envVars.BCRYPT_SALT_AROUND)
+  }
+
+  const newUpdateUser = await User.findByIdAndUpdate(userId, payload, {new: true, runValidators: true})
+
+  return newUpdateUser;
+}
 
 const getAllUsers = async () => {
   const users = await User.find({});
@@ -28,4 +93,5 @@ const getAllUsers = async () => {
 export const UserServices = {
   createUser,
   getAllUsers,
+  updateUser
 };
